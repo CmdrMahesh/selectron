65e866d5db5e4fb5f607fee720501f27375e22a3
CmdrMahesh <46396430+CmdrMahesh@users.noreply.github.com>
2022-01-04 17:14:10 +0530

    Update Controller.php

--- application/controller/publics/Controller.php
+++ application/controller/publics/Controller.php
@@ -4,21 +4,6 @@ namespace sp\selectron\controller\publics;
 defined( 'ABSPATH' ) || exit;
 
 abstract class Controller {
-
-	private static $_instance = null;
-
-	public static function instance() {
-		if ( ! isset( self::$_instance ) ) {
-			self::$_instance = new self;
-		}
-
-		return self::$_instance;
-	}
-
-	private function __construct() {
-		
-	}
-
-	public function hook_render();
-	public function js_render($selector,$delay);
+	abstract public function hook_render();
+	abstract public function js_render($selector,$delay);
 }
